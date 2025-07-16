8ce00c0d99f14dc5567a73ffc4c3b5b94337bd18
Andrés David Caro Mora <ddaviddcaro@gmail.com>
2025-07-15 21:53:17 -0500

    Creacion del chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import AppRoutes from './AppRoutes';
 import { CssBaseline, Container, ThemeProvider } from '@mui/material'; // 1. Importa ThemeProvider
 import theme from './theme'; // 2. Importa nuestro nuevo tema
+import Chatbot from './components/Chatbot'; 
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
             <footer className="App-footer">
               <p>&copy; {new Date().getFullYear()} Tu Marketplace Fresco.</p>
             </footer>
+            <Chatbot />
           </div>
         </BrowserRouter>
       </AuthProvider>
